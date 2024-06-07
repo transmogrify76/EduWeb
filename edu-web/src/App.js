c282ac185774c5237b70aed8efa3c1099a025173
sagniktransmogrify <transmogrify17@outlook.com>
2024-06-07 11:27:34 +0530

    push

--- edu-web/src/App.js
+++ edu-web/src/App.js
@@ -1,7 +1,9 @@
-import Login from "./components/login/login";
-import Signup from "./components/signup/signup";
-import Dashboard from "./components/dashboard/dashboard";
+import Contactus from "./components/Contactus/Contactus";
+import Adminview from "./components/Adminview/Adminview";
+import Login from "./components/Login/Login";
+import Dashboard from "./components/Dashboard/Dashboard";
 import Header from "./components/Header/Header";
+
 
 
 
@@ -9,7 +11,7 @@ import Header from "./components/Header/Header";
 function App() {
   return (
     <div >
-
+    <Login/>
     </div>
   );
 }
